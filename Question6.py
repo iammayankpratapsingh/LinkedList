6f3ef20d71a080d3f0882b48774f65ec9d21eecb
Mayank Pratap Singh <141426062+iammayankpratapsingh@users.noreply.github.com>
2023-12-05 23:36:41 +0530

    Create Question6.py

---
+++ Question6.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+    def middle(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.data
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
+print(linked_list.middle())
